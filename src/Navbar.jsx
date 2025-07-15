4d38bb76a38bcff9cd56302611839a77b1acf1da
rahulrp17 <rahulrp4021@gmail.com>
2025-07-15 09:11:22 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,8 +4,8 @@ import cloud from "./assets/cloud.png"
 const  Navbar = () => {
   return (
     <div className='navbar absolute top-0 left-0 right-0 shadow-lg flex bg-blue-600'>
-        <div className='flex items-center gap-2 py-3 ml-5'>
-             <img className='w-15 h-15' src={cloud} alt="" />
+        <div className='flex items-center gap-2 py-2 ml-5'>
+             <img className='w-13 h-13' src={cloud} alt="" />
             <h1 className='text-2xl font-bold  text-white'>Weather App</h1>
         </div>
       
